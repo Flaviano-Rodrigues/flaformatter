457a6c9c9498a1149f17c60d52175e288e7b2185
Flaviano Rodrigues <flavianodev@gmail.com>
2022-10-17 22:07:54 -0300

    added data formatter, maxLenth Option and return unformatted value

--- test.js
+++ test.js
@@ -1,13 +1,31 @@
 
+const { exit } = require('process')
 const readline = require('readline')
 const { flaformatter } = require('.')
 
 
 const question = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
+    completer: line => {
+        const completions = [
+            'rg',
+            'cpf/cnpj',
+            'cnpj',
+            'cpf',
+            'telefone',
+            'int',
+            'celular',
+            'data',
+            'cep',
+            'money',
+            'card',
+        ]
+        const hits = completions.filter((c) => c.startsWith(line))
+        // show all completions if none found
+        return [hits.length ? hits : completions, line]
+    }
 })
-
 
 let e = {
     target: {
@@ -18,16 +36,18 @@ let e = {
 
 let type = "rg"
 
-question.question('Type:', (res) => {
-    type = res
+question.question('Type: (rg) ', res => {
+    res != "" ? type = res : ''
 
-    question.question('Value:', (val) => {
-        e.target.value = val
+    question.question('Value: (123456789) ', val => {
+        val != '' ? e.target.value = val : ''
 
         flaformatter(e, type)
 
         console.log('\n', `Response: ${e.target.value}`, '\n')
 
+        exit()
     })
 
 })
+
